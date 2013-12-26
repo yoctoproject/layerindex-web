75c44fd770ffa1890159b516c7b0cc59669dea7e
Paul Eggleton <paul.eggleton@linux.intel.com>
2013-12-26 23:33:21 +0000

    Use Django sites framework to specify website title
    This allows specifying the site title in the database, instead of hardcoding
    it in the template. Just change it to something other than "example.com" to
    apply your own title.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- layerindex/context_processors.py
+++ layerindex/context_processors.py
@@ -5,10 +5,17 @@
 # Licensed under the MIT license, see COPYING.MIT for details
 
 from layerindex.models import Branch, LayerItem
+from django.contrib.sites.models import Site
 
 def layerindex_context(request):
+    site = Site.objects.get_current()
+    if site and site.name and site.name != 'example.com':
+        site_name = site.name
+    else:
+        site_name = 'OpenEmbedded metadata index'
     return {
         'all_branches': Branch.objects.exclude(name='oe-classic').order_by('sort_priority'),
         'unpublished_count': LayerItem.objects.filter(status='N').count(),
-        'oe_classic': Branch.objects.filter(name='oe-classic')
+        'oe_classic': Branch.objects.filter(name='oe-classic'),
+        'site_name': site_name
     }
