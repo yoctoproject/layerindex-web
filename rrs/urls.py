11df274e2bc5b757ab10fa4c55d39de376318ae5
Aníbal Limón <anibal.limon@linux.intel.com>
2015-01-09 09:49:22 -0600

    rrs: Add about page
    templates/base.html: Discard bulk change and submit layer when app is
    rrs.
    
    templates/rrs/about.html: Add about page that extends of base.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linux.intel.com>

--- rrs/urls.py
+++ rrs/urls.py
@@ -1,4 +1,20 @@
-from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.views.generic.simple import redirect_to
+from django.core.urlresolvers import reverse_lazy
+
+from django.conf.urls import patterns, url
+
+from layerindex.views import EditProfileFormView
 
 urlpatterns = patterns('',
+    url(r'^$', redirect_to, {'url' : reverse_lazy('about', args=())},
+        name='frontpage'),
+    url(r'^profile/$',
+        EditProfileFormView.as_view(
+        template_name='layerindex/profile.html'),
+        name="profile"),
+    url(r'^about/$',
+        TemplateView.as_view(
+        template_name='rrs/about.html'),
+        name="about"),
 )
