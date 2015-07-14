d8b6e988deeb7f4e601a81f4f411f6670ec5127f
Aníbal Limón <anibal.limon@linux.intel.com>
2015-07-14 17:36:45 -0500

    rrs/tools: Add daily_run.sh script for provide updates.
    Signed-off-by: Aníbal Limón <anibal.limon@linux.intel.com>

---
+++ rrs/tools/daily_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rrs_dir=__RRS_PATH__
+venv_activate=__VENV_ACTIVATE__
+
+source $venv_activate
+
+$rrs_dir/layerindex/update.py --reload
+$rrs_dir/rrs/tools/rrs_unique_recipes.py -d
+$rrs_dir/rrs/tools/rrs_maintainer_history.py -d
+$rrs_dir/rrs/tools/rrs_upgrade_history.py -d
+$rrs_dir/rrs/tools/rrs_upstream_history.py -d
+$rrs_dir/rrs/tools/rrs_distros.py -d
+
+if [ "$1" = "email" ]; then
+	$rrs_dir/rrs/tools/rrs_upstream_email.py
+fi
