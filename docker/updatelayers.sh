bf94b2f7e949ffbe806e69c8c4df1010dbf14265
Paul Eggleton <paul.eggleton@linux.intel.com>
2016-05-30 13:43:58 +1200

    update.py: allow updating all branches with one command
    Allow updating multiple branches, and if no branches are specified,
    update all branches that have a new "updates_enabled" flag field set to
    True. This avoids the need to have a separate shell script which runs
    update.py for each branch (and thus has hardcoded knowledge of each
    active branch in the index, i.e. it needs to be kept up-to-date in
    addition to the database.)
    
    The migration will default updates_enabled to True for all branches so
    if you wish to take advantage of this functionality, the flag will need
    to be set to False for any branches that shouldn't be updated.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- docker/updatelayers.sh
+++ docker/updatelayers.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 update=/opt/layerindex/layerindex/update.py
 $update -q -r
-$update -b jethro -x -r -q
-$update -b fido -x -r -q
-$update -b dizzy -x -r -q
-$update -b daisy -x -r -q
-$update -b dora -x -r -q
-$update -b dylan -x -r -q
