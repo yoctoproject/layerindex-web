yoctoproject/layerindex-web

commit c91372587bbddd4c595d7202e51a8740b787a06e

update.py: Allow bitbake to live in a subdirectory of a repository
Add a new BITBAKE_PATH to the settings file to specify the path within the
BITBAKE_REPO_URL where bitbake lives.  This is useful when using a combined
repository, such as poky, that contains bitbake, openembedded-core and other
layers.

This change also changes the default path, in the fetch directory, for the
bitbake checkout.  It no longer uses the path 'bitbake', but instead uses the
same URL processing as the layer fetching.

There is a side effect that, when using a shared fetch, the branch of the
layer will be used instead of the specified bitbake branch.  Generally this
is a reasonable compromise, since in a combined repository bitbake and
openembedded-core component should already match.

Signed-off-by: Mark Hatle <mark.hatle@kernel.crashing.org>
Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

layerindex/update.py
@@ -268,8 +268,6 @@ def main():
             logger.error("Layer index lock timeout expired")
             sys.exit(1)
         try:
-            bitbakepath = os.path.join(fetchdir, 'bitbake')
-
             if not options.nofetch:
                 # Make sure oe-core is fetched since recipe parsing requires it
                 layerquery_core = LayerItem.objects.filter(comparison=False).filter(name=settings.CORE_LAYER_NAME)
@@ -285,7 +283,17 @@ def main():
                     if layer.vcs_url not in allrepos:
                         allrepos[layer.vcs_url] = (repodir, urldir, fetchdir, layer.name)
                 # Add bitbake
-                allrepos[settings.BITBAKE_REPO_URL] = (bitbakepath, "bitbake", fetchdir, "bitbake")
+                if settings.BITBAKE_REPO_URL not in allrepos:
+                    bitbakeitem = LayerItem()
+                    bitbakeitem.vcs_url = settings.BITBAKE_REPO_URL
+                    bitbakeurldir = bitbakeitem.get_fetch_dir()
+                    bitbakepath = os.path.join(fetchdir, bitbakeurldir)
+                    allrepos[settings.BITBAKE_REPO_URL] = (bitbakepath, bitbakeurldir, fetchdir, "bitbake")
+
+                (bitbakepath, _, _, _) = allrepos[settings.BITBAKE_REPO_URL]
+                if getattr(settings, 'BITBAKE_PATH', ''):
+                    bitbakepath = os.path.join(bitbakepath, settings.BITBAKE_PATH)
+
                 # Parallel fetching
                 pool = multiprocessing.Pool(int(settings.PARALLEL_JOBS))
                 for url in allrepos:

docker/settings.py
@@ -244,6 +244,9 @@ TEMP_BASE_DIR = "/tmp"
 # Fetch URL of the BitBake repository for the update script
 BITBAKE_REPO_URL = "git://git.openembedded.org/bitbake"
 
+# Path within the BITBAKE_REPO_URL, usually empty
+BITBAKE_PATH = ""
+
 # Core layer to be used by the update script for basic BitBake configuration
 CORE_LAYER_NAME = "openembedded-core"
 

layerindex/bulkchange.py
@@ -98,7 +98,13 @@ def main():
 
     branch = utils.get_branch('master')
     fetchdir = settings.LAYER_FETCH_DIR
-    bitbakepath = os.path.join(fetchdir, 'bitbake')
+
+    import layerindex.models import LayerItem
+    bitbakeitem = LayerItem()
+    bitbakeitem.vcs_url = settings.BITBAKE_REPO_URL
+    bitbakepath = os.path.join(fetchdir, bitbakeitem.get_fetch_dir())
+    if getattr(settings, 'BITBAKE_PATH', ''):
+        bitbakepath = os.path.join(bitbakepath, settings.BITBAKE_PATH)
 
     if not os.path.exists(bitbakepath):
         sys.stderr.write("Unable to find bitbake checkout at %s" % bitbakepath)

layerindex/layerconfparse.py
@@ -20,7 +20,13 @@ class LayerConfParse:
 
         if not bitbakepath:
             fetchdir = settings.LAYER_FETCH_DIR
-            bitbakepath = os.path.join(fetchdir, 'bitbake')
+
+            from layerindex.models import LayerItem
+            bitbakeitem = LayerItem()
+            bitbakeitem.vcs_url = settings.BITBAKE_REPO_URL
+            bitbakepath = os.path.join(fetchdir, bitbakeitem.get_fetch_dir())
+            if getattr(settings, 'BITBAKE_PATH', ''):
+                bitbakepath = os.path.join(bitbakepath, settings.BITBAKE_PATH)
         self.bbpath = bitbakepath
 
         # Set up BBPATH.

layerindex/update_layer.py
@@ -300,7 +300,11 @@ def main():
         logger.error("Please set LAYER_FETCH_DIR in settings.py")
         sys.exit(1)
 
-    bitbakepath = os.path.join(fetchdir, 'bitbake')
+    bitbakeitem = LayerItem()
+    bitbakeitem.vcs_url = settings.BITBAKE_REPO_URL
+    bitbakepath = os.path.join(fetchdir, bitbakeitem.get_fetch_dir())
+    if getattr(settings, 'BITBAKE_PATH', ''):
+        bitbakepath = os.path.join(bitbakepath, settings.BITBAKE_PATH)
 
     layer = utils.get_layer(options.layer)
     urldir = layer.get_fetch_dir()

settings.py
@@ -239,6 +239,9 @@ TEMP_BASE_DIR = "/tmp"
 # Fetch URL of the BitBake repository for the update script
 BITBAKE_REPO_URL = "git://git.openembedded.org/bitbake"
 
+# Path within the BITBAKE_REPO_URL, usually empty
+BITBAKE_PATH = ""
+
 # Core layer to be used by the update script for basic BitBake configuration
 CORE_LAYER_NAME = "openembedded-core"
 

commit 8c33374d6cb1b03fc82582145165ec2f0f6d8818

update: fix exception with -x/--nofetch option
Fixes the bitbakepath variable not being defined with -x/--nofetch
specified.

(Regression introduced in c91372587bbddd4c595d7202e51a8740b787a06e.)

Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

layerindex/update.py
@@ -268,32 +268,32 @@ def main():
             logger.error("Layer index lock timeout expired")
             sys.exit(1)
         try:
+            # Make sure oe-core is fetched since recipe parsing requires it
+            layerquery_core = LayerItem.objects.filter(comparison=False).filter(name=settings.CORE_LAYER_NAME)
+            if layerquery_core in layerquery:
+                layerquery_fetch = list(layerquery)
+            else:
+                layerquery_fetch = list(layerquery) + list(layerquery_core)
+            # Fetch latest metadata from repositories
+            for layer in layerquery_fetch:
+                # Handle multiple layers in a single repo
+                urldir = layer.get_fetch_dir()
+                repodir = os.path.join(fetchdir, urldir)
+                if layer.vcs_url not in allrepos:
+                    allrepos[layer.vcs_url] = (repodir, urldir, fetchdir, layer.name)
+            # Add bitbake
+            if settings.BITBAKE_REPO_URL not in allrepos:
+                bitbakeitem = LayerItem()
+                bitbakeitem.vcs_url = settings.BITBAKE_REPO_URL
+                bitbakeurldir = bitbakeitem.get_fetch_dir()
+                bitbakepath = os.path.join(fetchdir, bitbakeurldir)
+                allrepos[settings.BITBAKE_REPO_URL] = (bitbakepath, bitbakeurldir, fetchdir, "bitbake")
+
+            (bitbakepath, _, _, _) = allrepos[settings.BITBAKE_REPO_URL]
+            if getattr(settings, 'BITBAKE_PATH', ''):
+                bitbakepath = os.path.join(bitbakepath, settings.BITBAKE_PATH)
+
             if not options.nofetch:
-                # Make sure oe-core is fetched since recipe parsing requires it
-                layerquery_core = LayerItem.objects.filter(comparison=False).filter(name=settings.CORE_LAYER_NAME)
-                if layerquery_core in layerquery:
-                    layerquery_fetch = list(layerquery)
-                else:
-                    layerquery_fetch = list(layerquery) + list(layerquery_core)
-                # Fetch latest metadata from repositories
-                for layer in layerquery_fetch:
-                    # Handle multiple layers in a single repo
-                    urldir = layer.get_fetch_dir()
-                    repodir = os.path.join(fetchdir, urldir)
-                    if layer.vcs_url not in allrepos:
-                        allrepos[layer.vcs_url] = (repodir, urldir, fetchdir, layer.name)
-                # Add bitbake
-                if settings.BITBAKE_REPO_URL not in allrepos:
-                    bitbakeitem = LayerItem()
-                    bitbakeitem.vcs_url = settings.BITBAKE_REPO_URL
-                    bitbakeurldir = bitbakeitem.get_fetch_dir()
-                    bitbakepath = os.path.join(fetchdir, bitbakeurldir)
-                    allrepos[settings.BITBAKE_REPO_URL] = (bitbakepath, bitbakeurldir, fetchdir, "bitbake")
-
-                (bitbakepath, _, _, _) = allrepos[settings.BITBAKE_REPO_URL]
-                if getattr(settings, 'BITBAKE_PATH', ''):
-                    bitbakepath = os.path.join(bitbakepath, settings.BITBAKE_PATH)
-
                 # Parallel fetching
                 pool = multiprocessing.Pool(int(settings.PARALLEL_JOBS))
                 for url in allrepos: